174af904a12e7a729039c552533668309cb5d235
Dasko7b <dyegodasko14@gmail.com>
2026-03-07 15:17:11 -0300

    feat:inserindo ms-cliente

---
+++ ms-cliente/src/main/java/br/dac/bantads/ms_cliente/MsClienteApplication.java
@@ -0,0 +1,13 @@
+package br.dac.bantads.ms_cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsClienteApplication.class, args);
+	}
+
+}
