5b1b5d8645971ff1866974d729f0fcc4387893b2
Thiago Cordeiro <thiagocordeiro2512@gmail.com>
2026-03-16 17:08:17 -0300

    feat: ultimas movimentações component

---
+++ bantads-ui/src/app/features/clientes/components/ultimas-movimentacoes/ultimas-movimentacoes.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UltimasMovimentacoes } from './ultimas-movimentacoes';
+
+describe('UltimasMovimentacoes', () => {
+  let component: UltimasMovimentacoes;
+  let fixture: ComponentFixture<UltimasMovimentacoes>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [UltimasMovimentacoes],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(UltimasMovimentacoes);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
