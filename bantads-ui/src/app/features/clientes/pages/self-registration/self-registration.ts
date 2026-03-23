f66d4ea49d78ab075734abf49635c3cf84c617dc
Albano Maywitz <allbano@gmail.com>
2026-03-23 16:16:24 -0300

    feat: add screen self-registration

---
+++ bantads-ui/src/app/features/clientes/pages/self-registration/self-registration.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-self-registration',
+  imports: [],
+  templateUrl: './self-registration.html',
+  styleUrl: './self-registration.css',
+})
+export class SelfRegistration {}
