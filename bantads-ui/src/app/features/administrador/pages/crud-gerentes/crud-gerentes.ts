e18f72de7e78a88ff005166f1a867ff52a862c38
Dasko7b <dyegodasko14@gmail.com>
2026-03-23 14:58:51 -0300

    feat: add dashboard, relatorio and crud-gerentes

---
+++ bantads-ui/src/app/features/administrador/pages/crud-gerentes/crud-gerentes.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crud-gerentes',
+  imports: [],
+  templateUrl: './crud-gerentes.html',
+  styleUrl: './crud-gerentes.css',
+})
+export class CrudGerentes {}
