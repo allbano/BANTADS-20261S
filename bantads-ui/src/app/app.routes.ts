f66e36e6b58b6fe12e20727c1d2c46ea4e238fbf
Matheus José <matheusjosclima@outlook.com>
2026-03-23 19:18:43 -0300

    feat: table and dashboard-gerente route

--- bantads-ui/src/app/app.routes.ts
+++ bantads-ui/src/app/app.routes.ts
@@ -1,9 +1,22 @@
 import { Routes } from '@angular/router';
+
+//Publicas
+
+//Clientes
 import { Dashboard } from './features/clientes/pages/dashboard/dashboard';
+
+//Gerentes
+import { DashboardGerente } from './features/gerente/pages/dashboard-gerente/dashboard-gerente'
+
+//Admins
 
 export const routes: Routes = [
     {
         path: '',
         component: Dashboard
+    },
+    {
+        path: 'dashboard-gerente',
+        component: DashboardGerente
     },
 ];
--- bantads-ui/src/app/features/gerente/pages/dashboard-gerente/dashboard-gerente.ts
+++ bantads-ui/src/app/features/gerente/pages/dashboard-gerente/dashboard-gerente.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, TemplateRef, ViewChild } from '@angular/core';
+import { Table } from "../../../../shared/components/table/table";
+
 
 @Component({
   selector: 'app-dashboard-gerente',
-  imports: [],
+  imports: [Table],
   templateUrl: './dashboard-gerente.html',
   styleUrl: './dashboard-gerente.css',
 })
-export class DashboardGerente {}
+export class DashboardGerente implements AfterViewInit{
+
+  @ViewChild('acoes', { static: true }) acoesTemplate!: TemplateRef<any>;
+
+  usuarios = [
+    { id: 1, nome: 'Matheus', idade: 20 },
+    { id: 2, nome: 'Ana', idade: 25 },
+  ];
+
+  colunas: any[] = [];
+
+  ngAfterViewInit(): void {
+  setTimeout(() => {
+    this.colunas = [
+      { id: 'nome', header: 'Nome' },
+      { id: 'idade', header: 'Idade' },
+      { id: 'acoes', header: 'Ações', render: this.acoesTemplate }
+    ];
+  });
+  }
+
+  onAction(event: any) {
+    console.log('Evento da tabela:', event);
+  }
+}
