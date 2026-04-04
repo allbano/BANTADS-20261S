de48c9593660460f8fe4ed814f83398e1d328679
Albano Maywitz <allbano@gmail.com>
2026-04-04 05:02:16 -0300

    refactor: update font and UX/UI of cliente/gerente/administrador

--- bantads-ui/src/app/features/cliente/components/credit-card/credit-card.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-credit-card',
-  imports: [],
-  templateUrl: './credit-card.html',
-  styleUrl: './credit-card.css',
-})
-export class CreditCard {}
--- bantads-ui/src/app/features/cliente/components/extract-feed/extract-feed.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-extract-feed',
-  imports: [],
-  templateUrl: './extract-feed.html',
-  styleUrl: './extract-feed.css',
-})
-export class ExtractFeed {}
