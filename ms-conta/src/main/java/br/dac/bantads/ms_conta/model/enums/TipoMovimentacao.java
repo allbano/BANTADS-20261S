f0cd027535ef4281a93b489220f324fd19fccb07
Dasko7b <dyegodasko14@gmail.com>
2026-04-20 19:25:29 -0300

    add: ModelConta, ModelMovimentação e dependencias no pom

---
+++ ms-conta/src/main/java/br/dac/bantads/ms_conta/model/enums/TipoMovimentacao.java
@@ -0,0 +1,10 @@
+package br.dac.bantads.ms_conta.model.enums;
+
+/**
+ * Tipos de movimentação bancária suportados pelo sistema BANTADS.
+ */
+public enum TipoMovimentacao {
+    DEPOSITO,
+    SAQUE,
+    TRANSFERENCIA
+}
