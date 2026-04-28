cdbd13021360a3fb9615065cd6532a2a49feb328
Matheus José <matheusjosclima@outlook.com>
2026-04-27 22:01:55 -0300

    fix: API Gateway

--- api-gateway/src/routes/index.js
+++ api-gateway/src/routes/index.js
@@ -4,16 +4,16 @@ const httpProxy = require('express-http-proxy');
 const helmet = require('helmet');
 const logger = require('morgan');
 const cors = require('cors');
-const verifyJWT = require('./middleware/auth'); 
+const verifyJWT = require('../middleware/auth'); 
 
 const app = express();
 const PORT = 40004; 
 
 // URLs dos microsserviços no Docker 
-const AUTH_URL = 'http://bantads-ms-auth:8085';
-const CLIENTE_URL = 'http://ms-cliente:8080';
-const CONTA_URL = 'http://ms-conta:8080';
-const FUNCIONARIO_URL = 'http://ms-funcionario:8080';
+const AUTH_URL = process.env.AUTH_URL || 'http://bantads-ms-auth:8085';
+const CLIENTE_URL = process.env.CLIENTE_URL || 'http://ms-cliente:8080';
+const CONTA_URL = process.env.CONTA_URL || 'http://ms-conta:8080';
+const FUNCIONARIO_URL = process.env.FUNCIONARIO_URL || 'http://ms-funcionario:8080';
 
 app.use(logger('dev'));
 app.use(helmet());
@@ -22,7 +22,7 @@ app.use(express.json());
 
 /* ROTAS PÚBLICAS */
 app.post('/login', httpProxy(AUTH_URL, {
-    proxyReqPathResolver: () => '/auth/login' // Mapeia para o endpoint real do ms-auth
+    proxyReqPathResolver: () => '/auth/login', // Mapeia para o endpoint real do ms-auth
 }));
 
 app.post('/autocadastro', httpProxy(CLIENTE_URL, {
@@ -32,9 +32,26 @@ app.post('/autocadastro', httpProxy(CLIENTE_URL, {
 /* ROTAS PROTEGIDAS
  * Exigem o header "Authorization: Bearer XXXXX" 
  */
-app.use('/clientes', verifyJWT, httpProxy(CLIENTE_URL));
-app.use('/contas', verifyJWT, httpProxy(CONTA_URL));
-app.use('/funcionarios', verifyJWT, httpProxy(FUNCIONARIO_URL));
+app.use('/clientes', verifyJWT, httpProxy(CLIENTE_URL, {
+    timeout: 8000, 
+    proxyErrorHandler: (err, res, next) => {
+        res.status(504).send({message: 'O serviço de clientes demorou a responder.'});
+    }
+}));
+
+app.use('/contas', verifyJWT, httpProxy(CONTA_URL, {
+    timeout: 8000, 
+    proxyErrorHandler: (err, res, next) => {
+        res.status(504).send({message: 'O serviço de contas demorou a responder.'});
+    }
+}));
+
+app.use('/funcionarios', verifyJWT, httpProxy(FUNCIONARIO_URL, {
+    timeout: 8000, 
+    proxyErrorHandler: (err, res, next) => {
+        res.status(504).send({message: 'O serviço de funcionários demorou a responder.'});
+    }
+}));
 
 app.listen(PORT, () => {
     console.log(`BANTADS API Gateway operando na porta ${PORT}`);
